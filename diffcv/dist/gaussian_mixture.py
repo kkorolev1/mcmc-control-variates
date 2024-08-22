1505f85b3303e110f656fd2774d983b75f524f8f
Kirill Korolev <kakorolev@edu.hse.ru>
2024-08-22 16:12:47 +0300

    refactor mixture code

--- diffcv/dist/gaussian_mixture.py
+++ diffcv/dist/gaussian_mixture.py
@@ -8,39 +8,62 @@ from diffcv.utils import fill_diagonal
 
 class GaussianMixture:
 
-    def __init__(self, means: jnp.ndarray, covs: jnp.ndarray, coeffs=None):
-        if coeffs is None:
-            coeffs = jnp.full((means.shape[0]), 1 / means.shape[0], dtype=jnp.float32)
-
-        assert means.shape[0] == covs.shape[0] == coeffs.shape[0]
-        assert jnp.allclose(sum(coeffs), 1)
-
-        if means.ndim == 1:
-            means = means[:, None]
-
-        if covs.ndim == 1:
-            covs = covs.reshape(means.shape[0], 1).repeat(means.shape[1], axis=1)
-
-        if covs.ndim < 3:
-            new_covs = jnp.zeros(
-                (means.shape[0], means.shape[1], means.shape[1]), dtype=jnp.float32
+    def __init__(
+        self,
+        component_means: jnp.ndarray,
+        component_covs: jnp.ndarray,
+        mixin_coeffs=None,
+    ):
+        if mixin_coeffs is None:
+            mixin_coeffs = jnp.full(
+                (component_means.shape[0]),
+                1 / component_means.shape[0],
+                dtype=jnp.float32,
             )
-            covs = fill_diagonal(new_covs, covs)
 
-        self.means = means
-        self.covs = covs
-        self.coeffs = coeffs
+        assert (
+            component_means.shape[0] == component_covs.shape[0] == mixin_coeffs.shape[0]
+        )
+        assert jnp.allclose(sum(mixin_coeffs), 1)
+
+        if component_means.ndim == 1:
+            component_means = component_means[:, None]
+
+        if component_covs.ndim == 1:
+            component_covs = component_covs.reshape(component_means.shape[0], 1).repeat(
+                component_means.shape[1], axis=1
+            )
+
+        if component_covs.ndim < 3:
+            new_covs = jnp.zeros(
+                (
+                    component_means.shape[0],
+                    component_means.shape[1],
+                    component_means.shape[1],
+                ),
+                dtype=jnp.float32,
+            )
+            component_covs = fill_diagonal(new_covs, component_covs)
+
+        self.component_means = component_means
+        self.component_covs = component_covs
+        self.mixin_coeffs = mixin_coeffs
 
     def log_prob(self, x):
-        log_ps = multivariate_normal.logpdf(x, self.means, self.covs)
-        return logsumexp(log_ps, b=self.coeffs)
+        log_ps = multivariate_normal.logpdf(
+            x, self.component_means, self.component_covs
+        )
+        return logsumexp(log_ps, b=self.mixin_coeffs)
 
     def sample(self, key: random.PRNGKey, n_samples: int):
-        mixture_size = len(self.coeffs)
+        mixture_size = len(self.mixin_coeffs)
         key1, key2 = random.split(key)
         mixture_component = random.randint(key1, (n_samples,), 0, mixture_size)
         samples = random.multivariate_normal(
-            key2, self.means, self.covs, shape=(n_samples, mixture_size)
+            key2,
+            self.component_means,
+            self.component_covs,
+            shape=(n_samples, mixture_size),
         )
         return jnp.take_along_axis(
             samples, mixture_component[:, None, None], axis=1
@@ -48,14 +71,22 @@ class GaussianMixture:
 
     @property
     def mean(self):
-        return jnp.dot(self.coeffs, self.means)
+        return jnp.dot(self.mixin_coeffs, self.component_means)
+
+    @property
+    def cov(self):
+        mean = self.mean
+        mean_var = (self.mixin_coeffs.reshape((-1, 1, 1)) * self.component_covs).sum(
+            axis=0
+        )
+        var_mean = (
+            self.mixin_coeffs.reshape((-1, 1, 1))
+            * jnp.einsum(
+                "ij,ik->ijk", self.component_means - mean, self.component_means - mean
+            )
+        ).sum(axis=0)
+        return mean_var + var_mean
 
     @property
     def variance(self):
-        mean = self.mean
-        mean_var = (self.coeffs.reshape((-1, 1, 1)) * self.covs).sum(axis=0)
-        var_mean = (
-            self.coeffs.reshape((-1, 1, 1))
-            * jnp.einsum("ij,ik->ijk", self.means - mean, self.means - mean)
-        ).sum(axis=0)
-        return mean_var + var_mean
+        return jnp.diag(self.cov)
